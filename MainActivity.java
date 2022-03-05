488bad259604202d0afe3cd175964c190ad71e81
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2022-03-05 10:43:02 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,52 @@
 package com.example.klicker;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public Integer counter = 0;
+    final String LOG_TAG = "myLogs";
+    Integer counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void onClickBtnAddStudents(View view) {
         counter++;
         TextView counterView = (TextView) findViewById(R.id.txt_counter);
         counterView.setText(counter.toString());
-        if(counter >= 1000){
-            TextView layout = (TextView)findViewById(R.id.txt_counter);
+        if (counter >= 1000) {
+            TextView layout = (TextView) findViewById(R.id.txt_counter);
             ViewGroup.LayoutParams params = layout.getLayoutParams();
             params.height = 200;
             params.height = 400;
             layout.setLayoutParams(params);
         }
-        if(counter >= 10000){
-            TextView layout = (TextView)findViewById(R.id.txt_counter);
+        if (counter >= 10000) {
+            TextView layout = (TextView) findViewById(R.id.txt_counter);
             ViewGroup.LayoutParams params = layout.getLayoutParams();
             params.height = 200;
             params.height = 600;
             layout.setLayoutParams(params);
         }
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("Count");
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Count", counter);
+        Log.d(LOG_TAG, "onSaveInstateState");
     }
 }
